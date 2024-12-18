2db0b7229afd787756291169b6eccbc86e91d7d6
Lyhou Phiv <lyhou282@gmail.com>
2024-12-18 22:17:45 +0700

    pull from voteu

--- components/PageComponent/PageComponent.tsx
+++ components/PageComponent/PageComponent.tsx
@@ -6,7 +6,7 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Recent } from "@/components/overview/RecentTraffic";
 import { FolderKanban, BookOpen, MessageSquare, UsersIcon } from "lucide-react";
 import { StatCard } from "@/components/overview/CardStat";
-import { Overview } from "@/components/overview/Overview";
+import { Overview } from "@/components/overview/overview";
 import { useCountBlogQuery } from '@/redux/service/blog';
 import { useCountUserQuery } from '@/redux/service/user';
 import { useCountFeedbackQuery } from '@/redux/service/feedback';
